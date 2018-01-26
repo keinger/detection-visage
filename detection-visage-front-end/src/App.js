a3bbae0ca65148a9f1b3d2e05423b4ebe12c418f
Moses <30348473+keinger@users.noreply.github.com>
2018-01-26 16:48:31 +0100

    Version 1.0.0

---
+++ detection-visage-front-end/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkFom";
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number :{
+      value : 30, 
+      density : {
+        enable :true,
+        value_area : 800
+      }
+    }
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Particles className="particles"
+          params={particlesOptions}
+            />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />{/*
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
