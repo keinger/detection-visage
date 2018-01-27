fb409b99886e4b254644fd560d8c4d5d53da58fe
Moses <30348473+keinger@users.noreply.github.com>
2018-01-27 15:13:15 +0100

    Ajout de l'api

--- detection-visage-front-end/src/App.js
+++ detection-visage-front-end/src/App.js
@@ -4,15 +4,21 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkFom";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'bbbe93bd21d84b709b5816e577415c34'
+});
 
 const particlesOptions = {
   particles: {
-    number :{
-      value : 30, 
-      density : {
-        enable :true,
-        value_area : 800
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
       }
     }
   }
@@ -20,17 +26,47 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl:''
+
+    }
+  }
+
+  onInputChange = (event) => {
+
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl : this.state.input});
+    // renvoit à l'api de Clarifai, afin d'analyser l'image  
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {
+        
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Particles className="particles"
           params={particlesOptions}
-            />
+        />
         <Logo />
         <Rank />
-        <ImageLinkForm />{/*
-        <FaceRecognition />*/}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl ={this.state.imageUrl} />
       </div>
     );
   }
